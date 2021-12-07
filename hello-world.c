5f740ca2ffda6d51a3ed83b6faf79064d5f8cba3
kneeziefbaby <93338478+kneeziefbaby@users.noreply.github.com>
2021-12-06 19:31:56 -0700

    Create hello-world.c

---
+++ hello-world.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // printf() displays the string inside quotation
+    printf("Hello World in C from Github, better late than never!");
+    return 0;
+}
+
+//To run this command in terminal you will need to run the following command:
+//gcc hello-world.c -o hello-world-in-c
+
